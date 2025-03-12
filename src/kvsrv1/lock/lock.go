8b08cb9cdb902484629635dca10bfd279df3a348
zjw <zjw1858@163.com>
2025-03-12 18:33:21 +0800

    pass lock_test reliable

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,11 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	// "log"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -11,6 +15,9 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+
+	id       string
+	lock_key string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +26,52 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{ck: ck,
+		id:       kvtest.RandValue(8),
+		lock_key: l,
+	}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		val, ver, err:= lk.ck.Get(lk.lock_key)
+
+		if val == "" || err == rpc.ErrNoKey{
+			err = lk.ck.Put(lk.lock_key, lk.id, ver)
+			if err == rpc.OK {
+				// log.Printf("%s gets lock\n", lk.id)
+				return 
+			}
+		}
+
+		// sleep for a while
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+
+	for {
+		val, ver, err := lk.ck.Get(lk.lock_key)
+		if err != rpc.OK {
+			panic("fail to check lock state\n")
+		}
+
+		if val != lk.lock_key {
+			// log.Printf("try to release other's lock\n")
+			time.Sleep(10 * time.Millisecond)
+		}
+
+		if val != "" {
+			err = lk.ck.Put(lk.lock_key, "", ver)
+			if err == rpc.OK {
+				// log.Printf("%s unlock\n", lk.id)
+				return 
+			}
+		}
+	}
 }
