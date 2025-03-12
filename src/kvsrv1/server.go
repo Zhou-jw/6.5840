24de80671bd648be308486b29c605526c2993daa
zjw <zjw1858@163.com>
2025-03-12 14:18:29 +0800

    pass reliable_test

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,15 +18,22 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type ValueVer struct {
+	Value   string
+	Version rpc.Tversion
+}
 
 type KVServer struct {
-	mu sync.Mutex
-
+	rw       sync.RWMutex
+	mapinner map[string]ValueVer
 	// Your definitions here.
 }
 
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
+	kv := &KVServer{
+		rw:       sync.RWMutex{},
+		mapinner: make(map[string]ValueVer),
+	}
 	// Your code here.
 	return kv
 }
@@ -35,6 +42,16 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.rw.RLock()
+	defer kv.rw.RUnlock()
+
+	if valueVer, ok := kv.mapinner[args.Key]; ok {
+		reply.Value = valueVer.Value
+		reply.Version = valueVer.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,12 +60,42 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.rw.Lock()
+	defer kv.rw.Unlock()
+
+	valueVer, is_exist := kv.mapinner[args.Key]
+	if !is_exist {
+		switch args.Version {
+		case 0:
+			kv.mapinner[args.Key] = ValueVer{
+				Value:   args.Value,
+				Version: args.Version + 1,
+			}
+			reply.Err = rpc.OK
+		default:
+			reply.Err = rpc.ErrNoKey
+		}
+		return
+	}
+
+	// key already exists, with mismatched version
+	if valueVer.Version != args.Version {
+		reply.Err = rpc.ErrVersion
+		return
+	}
+
+	// version match, update value
+	kv.mapinner[args.Key] = ValueVer{
+		Value:   args.Value,
+		Version: args.Version + 1,
+	}
+	reply.Err = rpc.OK
+
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
